f67bafda9417082c3fc4ebfa99d46c3fc35d2c52
Lien Kim <lienkt0110@gmail.com>
2025-08-02 17:45:58 +0700

    init

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+let data = {
+  our_album: "Our Albums",
+  pre_wedding_title: "Album Pre-Wedding – Cột mốc đẹp giữa Paris",
+  pre_wedding_content: `Ngày 3 tháng 4 năm 2025, 
+  chúng tôi trở lại Paris để chụp ảnh pre-wedding cho đám cưới.
+  Năm 2025 đánh dấu những cột mốc đẹp và ý nghĩa: 10 năm bên nhau, 
+  cả hai vợ chồng cùng bước sang tuổi 30, 
+  và có Gấu – con trai đầu lòng – lúc này con vừa được 7 tháng tuổi. 
+  May mắn có thể lưu giữ lại 
+  bằng những khoảnh khắc đáng nhớ giữa thành phố gắn với nhiều kỷ niệm tuổi trẻ này.
+  Hai đứa đều học master và Nhân thì làm phd ở đây. 
+  Dù khởi đầu cùng nhau vào thời điểm bùng phát dịch COVID-19 năm 2020 
+  nên cuộc sống những năm tháng đó cũng không bình thường suôn sẻ 
+  nhưng những kỉ niệm tươi đẹp vẫn luôn hiện lên rõ nét trong lòng.
+  Mùa xuân hoa anh đào, tulip, thuỷ tiên nở khắp nơi, mùa thu lá vàng, 
+  mùa đông có những ngày tuyết rơi ngập đường, những tối đi làm về ngắm tháp Effel lên đèn.
+Quay trở lại Paris lần này, không còn là hai người trẻ nữa, mà là một gia đình nhỏ ba người, 
+cảm thấy thời gian như khép lại một vòng tròn thật trọn vẹn. 
+Bộ ảnh này không chỉ ghi lại hình ảnh, mà còn đánh dấu một hành trình dài
+ – của tình yêu, trưởng thành và hạnh phúc.`,
+};
+
+let vnData = {
+  our_album: "Our Albums",
+  pre_wedding_title: "Our Pre-Wedding Album – A Beautiful Milestone in Paris",
+  pre_wedding_content: `On April 3rd, 2025, we returned to Paris to capture a very special moment — our own pre-wedding photos.
+
+That day marked a beautiful milestone: 10 years together, both of us turning 30, and our first son, Gấu, reaching 7 months old. The round and meaningful numbers inspired us to preserve this time with heartfelt memories in a city filled with our shared youth.
+
+Paris holds a sweet place in our hearts. At 23, my husband came here to pursue his Master’s degree. In 2020, I joined him for my own studies while he began his PhD. Though the world was going through the COVID-19 pandemic then, we experienced some of the most beautiful years of our lives here.
+
+This time, we returned not as a young couple, but as a family of three. It felt like life had come full circle. This photo session wasn’t just about taking pictures — it was our way of honoring a long journey of love, growth, and joy.`,
+};
